61e24f9215fff3bda0dc753c80db04f3bb9a970c
Carlos Mendes <carlos.antunes.mendes@gmail.com>
2019-09-03 09:13:11 -0300

    Inclusão validações Post e Put Categoria

---
+++ src/main/java/com/cmendes/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,45 @@
+package com.cmendes.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.cmendes.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5,max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	private String name;
+	
+	public CategoriaDTO() {
+	}
+
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
--- src/main/java/com/cmendes/cursomc/services/CategoriaService.java
+++ src/main/java/com/cmendes/cursomc/services/CategoriaService.java
@@ -1,14 +1,17 @@
 package com.cmendes.cursomc.services;
 
-import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
 import java.util.Optional;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cmendes.cursomc.domain.Categoria;
+import com.cmendes.cursomc.dto.CategoriaDTO;
 import com.cmendes.cursomc.repositories.CategoriaRepository;
 import com.cmendes.cursomc.services.exceptions.DataViolationException;
 import com.cmendes.cursomc.services.exceptions.ObjectNotFoundException;
@@ -44,5 +47,20 @@ public class CategoriaService {
 		}catch (DataIntegrityViolationException e) {
 			throw new DataViolationException("Não e possível excluir uma categoria que possui produto");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		List<Categoria> list = repo.findAll();
+		return list;
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page,linesPerPage,Direction.valueOf(direction) , orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+
+		return new Categoria(objDto.getId(), objDto.getName());
 	}
 }
